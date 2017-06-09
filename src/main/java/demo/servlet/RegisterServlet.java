ef2659abdff7d3b97006b1c1d51c9fe3329b7320
mingfei <mingfei.net@gmail.com>
2017-06-09 17:20:33 +0800

    Update

--- src/main/java/demo/servlet/RegisterServlet.java
+++ src/main/java/demo/servlet/RegisterServlet.java
@@ -3,7 +3,6 @@ package demo.servlet;
 import demo.util.Db;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
