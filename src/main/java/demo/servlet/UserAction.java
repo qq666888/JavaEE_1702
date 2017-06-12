6edf16dc13b54cdd3319a946089c481ce40a3be9
mingfei <mingfei.net@gmail.com>
2017-06-12 09:42:03 +0800

    Update

--- src/main/java/demo/servlet/UserAction.java
+++ src/main/java/demo/servlet/UserAction.java
@@ -18,10 +18,20 @@ public class UserAction extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String action = req.getParameter("action");
         System.out.println(action);
+        if (action.equals("register")) {
+            // TODO: 6/12/17 register
+        } else if (action.equals("login")) {
+            // TODO: 6/12/17 login
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("do get...");
+        String action = req.getParameter("action");
+        if (action.equals("logout")) {
+            req.getSession().invalidate();
+            resp.sendRedirect("index.jsp");
+        } else {
+        }
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,7 +12,7 @@
 </head>
 <body>
 <h1>登录<%=session.getId()%></h1>
-<form action="user" method="post">
+<form action="user">
     <input type="hidden" name="action" value="login">
     <input type="text" name="mobile" placeholder="手机号"><br>
     <input type="password" name="password" placeholder="密码"><br>
